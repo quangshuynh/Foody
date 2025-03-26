de2b96a5c82092781ac1d0b767acf93511995def
Quang <qth9368@rit.edu>
2025-03-26 17:42:36 -0400

    firebase init change

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AYO",  // put your own API key here
+  apiKey: "AIzaSyD6gCXwMNBqXF7DtDLGX1RYCB6COetDC5c",  // put your own API key here
   authDomain: "foody-e5677.firebaseapp.com",
   projectId: "foody-e5677",
   storageBucket: "foody-e5677.firebasestorage.app",
@@ -15,9 +17,6 @@ const firebaseConfig = {
   appId: "1:981213113044:web:15cecd02989f2608b56941",
   measurementId: "G-4NBS6J9DLG"
 };
-
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
