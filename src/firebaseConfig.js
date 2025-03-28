25fb0f05fe4c7f13fb82252035dd83901d7f8c1a
forkei <olivier.couthaud@gmail.com>
2025-03-27 20:32:28 -0400

    chore: Remove Firebase configuration values

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AYO",  // put your own API key here
-  authDomain: "foody-e5677.firebaseapp.com",
-  projectId: "foody-e5677",
-  storageBucket: "foody-e5677.firebasestorage.app",
-  messagingSenderId: "981213113044",
-  appId: "1:981213113044:web:15cecd02989f2608b56941",
-  measurementId: "G-4NBS6J9DLG"
+  apiKey: "",  // put your own API key here
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
